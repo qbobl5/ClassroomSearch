52e9baf0598472b776211134e8542e8a62bb1b28
qbobl5 <ddddd991125@gmail.com>
2022-10-07 19:29:17 +0900

    First Commit

---
+++ app/src/main/java/com/example/hallymsmartapp/Classroom.java
@@ -0,0 +1,108 @@
+package com.example.hallymsmartapp;
+
+public class Classroom { // 강의실 변수 클래스
+
+    private Object roomName; // 강의실 이름 변수 ex)1212, 10312
+    public Classroom(Object n) { // 강의실 변수 클래스 생성자
+        this.roomName=n;
+    }
+
+    public void setRoomName(Object n) {
+        this.roomName=n;
+    }
+
+    public Object getRoomName() {
+        return roomName;
+    }
+
+    public String building() { // 강의실 이름을 받아온 뒤 건물명 찾기
+        if (roomName.toString().charAt(0) == 'A') {
+            // A로 시작하는 건물 표시
+            return null;
+        }
+
+        switch (Integer.parseInt(roomName.toString()) / 1000) {
+            case 1:
+                return "공학관";
+            case 2:
+                return "대학본부-인문1관";
+            case 3:
+                return "의학관";
+            case 4:
+                return "인문2관";
+            case 5:
+                return "대학본부별관";
+            case 6:
+                return "실험동물센터";
+            case 7:
+                return "자연과학관";
+            case 8:
+                return "생명과학관";
+            case 9:
+                return "CLC";
+            case 10:
+                return "사회경영1관";
+            case 11:
+                return "일송아트홀";
+            case 12:
+                return "창업보육센터";
+            case 13:
+                return "사회경영2관";
+            case 14:
+                return "국제관";
+            case 15:
+                return "국제회의관";
+            case 16:
+                return "기초교육관";
+            case 17:
+                return "일송기념도서관";
+            case 18:
+                return "한림레크레이션센터";
+            case 19:
+                return "학군단";
+            case 20:
+                return "실내테니스장";
+            case 21:
+                return "의료바이오융합연구원";
+            case 22:
+                // 산학협력관
+                return null;
+            case 23:
+                return "도현글로벌스쿨";
+            case 24:
+                return "학생생활관 1관";
+            case 25:
+                return "학생생활관 2관";
+            case 26:
+                return "학생생활관 3관";
+            case 27:
+                return "학생생활관 4관";
+            case 28:
+                return "학생생활관 5관";
+            case 29:
+                return "학생생활관 6관";
+            case 30:
+                return "학생생활관 7관";
+            case 31:
+                return "학생생활관 8관";
+            case 32:
+                // 체육 기자재실
+                return null;
+            case 33:
+                // H Stadium
+                return null;
+            case 34:
+                // ILSONG Studium
+                return null;
+            case 35:
+                // 씨름장
+                return null;
+            case 36:
+                // 온실
+                return null;
+            default:
+                return "존재하지 않습니다.";
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/hallymsmartapp/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.hallymsmartapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    MenuItem mSearch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //메뉴 생성하는 onCreateOptionsMenu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        //search_menu.xml 등록
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.search_menu,menu);
+        mSearch=menu.findItem(R.id.search);
+
+
+
+        //menuItem을 이용해서 SearchView 변수 생성
+        SearchView sv=(SearchView)mSearch.getActionView();
+
+        //SearchView의 검색 이벤트
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            //검색버튼을 눌렀을 경우
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                TextView text = (TextView)findViewById(R.id.txtresult);
+                Classroom key=new Classroom(query);
+                String value=key.building();
+                text.setText(query+" : "+value);
+                return true;
+            }
+
+            //텍스트가 바뀔때마다 호출
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                TextView text = (TextView)findViewById(R.id.txtsearch);
+                text.setText("검색식 : "+newText);
+                return true;
+            }
+        });
+        return true;
+    }
+
+}
